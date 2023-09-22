710bce981e8499a045714e7cce61a418d08a42a3
ATPCOXBAU <93449158+ATPCOXBAU@users.noreply.github.com>
2023-09-22 04:13:25 -0600

    a

---
+++ script.js
@@ -0,0 +1,10 @@
+function alerta() {  
+    alert("Hola Ali!");
+    alert("No pude darte esto fisico acepta esto :)")
+    alert("Es lo mas que pude hacer en este tiempito")
+    alert("De Cox uwu")
+    alert("")
+    //abre una ventana nueva con este link
+    //"https://www.youtube.com/watch?v=INTW7_UPA8I"
+    window.open("https://www.youtube.com/watch?v=INTW7_UPA8I");
+}
